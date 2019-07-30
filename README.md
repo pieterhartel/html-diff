ba5e0a066c66c53e75e4607ebe88c693d4ed07f1
Quentin Wenger <quentin.wenger@oly-exams.org>
2019-07-30 20:55:02 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,49 @@
+# HTML-Diff: HTML-formatted diff'ing of HTML snippets
+
+Compares two HTML snippets strings and returns the diff as a valid HTML snippet with changes wrapped in <del> and <ins> tags.
+
+Relies on ```BeautifulSoup4``` with ```html.parser``` backend for HTML parsing and dumping.
+
+
+## Usage
+
+```python
+>>> from html_diff import diff
+>>> diff("<em>ABC</em>", "<em>AB</em>C")
+'<em>AB<del>C</del></em><ins>C</ins>'
+```
+
+
+## Testing
+
+### Automated testing
+
+Run
+
+```bash
+python -m unittest
+```
+
+at the root of the project.
+
+
+### Manual testing
+
+Run
+
+```bash
+python -m html_diff
+```
+
+at the root of the project and navigate to 127.0.0.1:8080 with your browser.
+
+You can specify further options:
+
+- ```-a``` or ```--address```: custom address of the server (default: 127.0.0.1)
+- ```-p``` or ```--port```: custom port of the server (default: 8080)
+- ```-b``` or ```--blocks```: definitions of functions to be added to tags_fcts_as_blocks, e.g. for treating ```<span class="math-tex">\(...\)</span>``` as insecable blocks:
+
+```python
+lambda tag: tag.name == "span" and "math-tex" in tag.attrs.get("class", [])
+```
+
---
+++ tests/test_html_diff.py
@@ -0,0 +1,42 @@
+import pathlib
+import re
+import unittest
+
+from html_diff import __version__
+from html_diff import diff
+from html_diff.check import old_from_diff
+from html_diff.check import new_from_diff
+
+
+
+test_diffs = (
+    (
+        "<ul><li>ax</li><li>b</li><li>c</li></ul>",
+        "<ul><li>a</li><li>by</li><li>c</li></ul>",
+        "<ul><li>a<del>x</del></li><li>b<ins>y</ins></li><li>c</li></ul>",
+    ),
+    (
+        "<em>a</em><em>bb</em>",
+        "<em>bb</em><em>a</em>",
+        "<del><em>a</em></del><em>bb</em><ins><em>a</em></ins>",
+    ),
+)
+
+
+class TestHTMLDiff(unittest.TestCase):
+    def test_version(self):
+        with open(pathlib.Path(__file__).parent.parent / "pyproject.toml") as fi:
+            version_match = re.search(r'version\s*=\s*"(.+?)"', fi.read())
+        self.assertNotEqual(version_match, None)
+        self.assertEqual(version_match.group(1), __version__)
+    def test_diff(self):
+        for old, new, df in test_diffs:
+            d = diff(old, new)
+            self.assertEqual(d, df)
+            self.assertEqual(old, old_from_diff(d))
+            self.assertEqual(new, new_from_diff(d))
+
+
+if __name__ == "__main__":
+    unittest.main()
+
